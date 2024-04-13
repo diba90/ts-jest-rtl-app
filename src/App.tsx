aa643b328f007cb7b3a9f14b191160dbcae3a5fd
Dibakar <dibakar1290@gmail.com>
2024-04-13 22:12:53 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+import { ThemeContext } from './ThemeProvider';
 
 function App() {
+
+   const { theme, toggleTheme } = useContext(ThemeContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      <div>Hi friend!</div>
+      <button onClick={toggleTheme}>
+        Switch to {theme === 'light' ? 'dark' : 'light'} mode
+      </button>
     </div>
   );
 }
